4593894c49bbc4802e122aff4dbbb5a10272e360
Kristyan <kristyan@joltonline.com>
2011-02-23 12:57:35 +0000

    Added some config and validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :articles, :dependent => :destroy
+  
+  validates :name, :presence => true, :length => {:minimum => 4}
 end
---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,5 @@
+RailsAdmin.authenticate_with {}
+
+RailsAdmin.config do |config|
+  config.navigation.max_visible_tabs 3
+end
